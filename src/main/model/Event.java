cd36ac5223a116bb47cb6984831992e8f03e427d
Shahrukh <shahruk@students.cs.ubc.ca>
2021-11-25 06:45:51 -0800

    Part 4 event log corrected

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -9,7 +9,6 @@ import java.util.Date;
  */
 public class Event {
     private static final int HASH_CONSTANT = 13;
-    private Date dateLogged;
     private String description;
 
     /**
@@ -19,17 +18,7 @@ public class Event {
      * @param description a description of the event
      */
     public Event(String description) {
-        dateLogged = Calendar.getInstance().getTime();
         this.description = description;
-    }
-
-    /**
-     * Gets the date of this event (includes time).
-     *
-     * @return the date of the event
-     */
-    public Date getDate() {
-        return dateLogged;
     }
 
     /**
@@ -51,16 +40,16 @@ public class Event {
         }
         Event otherEvent = (Event) other;
 
-        return ((this.dateLogged.equals(otherEvent.dateLogged)) && this.description.equals(otherEvent.description));
+        return (this.description.equals(otherEvent.description));
     }
 
     @Override
     public int hashCode() {
-        return (HASH_CONSTANT * dateLogged.hashCode() + description.hashCode());
+        return  description.hashCode();
     }
 
     @Override
     public String toString() {
-        return dateLogged.toString() + "\n" + description;
+        return   "\n" + description;
     }
 }
